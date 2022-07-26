85a64b3317fb1a17f70dd53d56735343f03e24eb
Manasask8127 <manasask8127@gmail.com>
2022-07-26 11:32:25 +0530

    added dependency

--- app/src/main/java/com/udacity/project4/locationreminders/data/local/RemindersDao.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/data/local/RemindersDao.kt
@@ -4,7 +4,6 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import androidx.room.Update
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 
 /**
@@ -16,14 +15,14 @@ interface RemindersDao {
      * @return all reminders.
      */
     @Query("SELECT * FROM reminders")
-    suspend fun getReminders(): List<ReminderDTO>
+    fun getReminders(): List<ReminderDTO>
 
     /**
      * @param reminderId the id of the reminder
      * @return the reminder object with the reminderId
      */
     @Query("SELECT * FROM reminders where entry_id = :reminderId")
-    suspend fun getReminderById(reminderId: String): ReminderDTO?
+    fun getReminderById(reminderId: String): ReminderDTO?
 
     /**
      * Insert a reminder in the database. If the reminder already exists, replace it.
@@ -31,12 +30,12 @@ interface RemindersDao {
      * @param reminder the reminder to be inserted.
      */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun saveReminder(reminder: ReminderDTO)
+    fun saveReminder(reminder: ReminderDTO)
 
     /**
      * Delete all reminders.
      */
     @Query("DELETE FROM reminders")
-    suspend fun deleteAllReminders()
+    fun deleteAllReminders()
 
 }
